726d6f8472ca3659e14e118d40e37260b41c0315
Rohith-Kannan-M <46962744+Rohith-Kannan-M@users.noreply.github.com>
2020-09-25 02:38:10 +0530

    Add files via upload

---
+++ PHP-DB file/unistamgroups_yypizza.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 24, 2020 at 11:38 PM
+-- Server version: 10.3.24-MariaDB-cll-lve
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `unistamgroups_yypizza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `ord_id` varchar(200) NOT NULL,
+  `items` varchar(2000) NOT NULL,
+  `name` varchar(110) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `ord_status` varchar(20) NOT NULL DEFAULT 'accepted'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
